5807e800d14274084f364041a420b8bd26fb6c76
YoussraJ <yosrajamai10@gmail.com>
2023-05-31 14:40:27 +0200

    UPD

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
+import './App.css';
 
 function App() {
   return (
+    <AppProvider>
     <div className="App">
+      <h1>Shopping App</h1>
+      <div style={{display:'flex',gap:'20px'}}> 
+      <CartValue className="item" />
+      <Location className="item"/>
+      </div>
+    
+      <ExpenseList />
+      <ItemSelected />
     </div>
+    </AppProvider>
   );
 }
 
